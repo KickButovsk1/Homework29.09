1ea32dada42cd360b4413c59773bce20a4bc1fcb
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-09-28 09:12:51 +0500

    Add files via upload

---
+++ Classwork2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string str = "Это тест";
+            string s2 = "это простой тест";
+            s2 = str.Replace(str, s2);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+
+        }
+    }
+}
