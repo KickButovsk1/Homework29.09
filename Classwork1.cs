111275ffb2d38778ae0ec82650f34fa386fa2209
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-09-28 09:25:01 +0500

    Classwork1.cs

--- Classwork1.cs
+++ Classwork1.cs
@@ -17,21 +17,27 @@ namespace BoolProject
             string str2 = "о";
             string str3 = "ми";
             string str4 = "а";
+            string str5 = "б";
+            string str6 = "в";
             bool x = str.StartsWith("C# обладает");
             bool y = str.EndsWith("строк");
             int i = str.IndexOf(str2);
             int i2 = str.LastIndexOf(str2);
             int i3 = str.IndexOf(str3);
             int i4 = str.LastIndexOf(str3);
+            int i5 = str.IndexOf(str4);
+            int i6 = str.IndexOf(str5);
+            int i7 = str.IndexOf(str6);
+            int number = Math.Min(i5, i6);
+            int number1 = Math.Min(i6, i7);
+            int number3 = Math.Min(number, number1);
             Console.WriteLine(i);
             Console.WriteLine(i2);
             Console.WriteLine(i3);
             Console.WriteLine(i4);
+            Console.WriteLine(number3);
             Console.WriteLine(x);
             Console.WriteLine(y);
-            Console.WriteLine(Math.Min(i, i2));
-            Console.WriteLine(Math.Min(i2, i3));
-            Console.WriteLine(Math.Min(i3, i4));
             Console.ReadKey();
 
         }
