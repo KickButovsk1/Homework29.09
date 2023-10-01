521f631767f99aa1627288bceb682211763ed63f
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-01 22:23:53 +0500

    Add files via upload

---
+++ HomeWork 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "это простой тест";
+            Console.WriteLine($"Исходная строка: " + s1);
+            string s2 = s1.Replace("простой", "непростой");
+            Console.WriteLine($"Новая строка: " + s2);
+            string s3 = s2.Replace("непростой", "нопростой");
+            string s4 = s3.Replace("тест","тост");
+            Console.WriteLine($"Новая строка: " + s4);
+            Console.ReadKey();
+        }
+    }
+}
