2454bdb1fc333f8ef1cd0ad706bbef23dbbdd4ce
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-01 23:48:17 +0500

    Add files via upload

---
+++ HomeWork 5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "(вайпернаин)майскайангел";
+            int first = s.IndexOf("(");
+            Console.WriteLine(first);
+            int second = s.IndexOf(")");
+            Console.WriteLine(second);
+            if (first < second)
+            {
+                string slovo = s.Substring(first + 1, second - first - 1);
+                Console.WriteLine("Все символы, расположенные между скобками: " + slovo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork 6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "вайп::ерв!!се";
+            string str1 = ":";
+            string str2 = "!";
+            int dlinna = str.Length;
+            int first = dlinna / 2;
+            string p = str.Substring(0, first);
+            string p1 = str.Substring(first);
+            if (p.Contains(":") && p1.Contains("!"))
+            {
+                string r = str.Replace(":", "*");
+                string r2 = r.Replace("!", ".");
+                Console.WriteLine("Cтрока -  " + r2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
